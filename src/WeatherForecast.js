53e1c4adb96e3b0ce308484de8b62c16a61541e9
Rolanda-Davids-97 <davidsrolanda@yahoo.com>
2024-05-03 22:59:04 +0200

    created the forecast for the first day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`;
+
+    axios
+      .get(apiUrl)
+      .then(handleResponse)
+      .catch((error) => console.error("Error fetching weather data:", error));
+  }, [props.coordinates]);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "9956c41def8fc21e1cc4d9b6ebba73d6";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  if (!loaded) {
+    return null; // Return null or loading indicator while data is being fetched
+  }
 
-  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          <div className="WeatherForecast-day">Thu </div>
-          <WeatherIcon code="01d" size={50} />
-          <div className="WeatherForceast-temperature">
-            <span className="WeatherForecast-temperature-max"> 19°</span>
-            <span className="WeatherForescast-temperature-min">10°</span>
-          </div>
+          <WeatherForecastDay data={forecast[0]} />
         </div>
       </div>
     </div>
